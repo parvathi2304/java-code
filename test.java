ca326b4f6094068c06417bb1bd3165760bdf4536
parvathi2304 <shyinthavi@gmail.com>
2025-02-07 20:32:15 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
       System.out.println("*********************");
-        System.out.println("Hello, its commited & changed!!!");
+        System.out.println("Hello, changing!!!");
        System.out.println("*********************");
     }
 }
