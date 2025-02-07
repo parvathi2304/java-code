ecf2a641bae508241d13954a3fccae2d59f56865
parvathi2304 <shyinthavi@gmail.com>
2025-02-07 20:37:56 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
       System.out.println("*********************");
-        System.out.println("This is changed in GITHub!!!");
+        System.out.println("in GITHub!!!");
        System.out.println("*********************");
     }
 }
