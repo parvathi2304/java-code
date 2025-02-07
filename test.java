110cf33cfa8e942581c0f09313fcf1b7b7889ebd
parvathi2304 <shyinthavi@gmail.com>
2025-02-07 19:17:55 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class test {
     public static void main(String[] args) {
       System.out.println("*********************");
         System.out.println("Hello, welcome to the GIT!");
